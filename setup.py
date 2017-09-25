91d4dbdd4023d85a1727aa6adbb7bc4dc7e80e68
colan connon <cconnon11@gmail.com>
2017-09-24 20:25:09 -0400

    redo setup file

--- setup.py
+++ setup.py
@@ -1,58 +1,15 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import os
-import re
-import sys
-
 from setuptools import setup
-
-
-def get_version(package):
-    """
-    Return package version as listed in `__version__` in `init.py`.
-    """
-    init_py = open(os.path.join(package, '__init__.py')).read()
-    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
-
-
-def get_packages(package):
-    """
-    Return root package and all sub-packages.
-    """
-    return [dirpath
-            for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-
-def get_package_data(package):
-    """
-    Return all files under the root package, that are not in a
-    package themselves.
-    """
-    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
-            for dirpath, dirnames, filenames in os.walk(package)
-            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
-    filepaths = []
-    for base, filenames in walk:
-        filepaths.extend([os.path.join(base, filename)
-                          for filename in filenames])
-    return {package: filepaths}
-
-
-version = get_version('apistar_alembic_migrations')
 
 
 setup(
     name='apistar_alembic_migrations',
     version='0.0.1',
     license='MIT',
+    url="https://github.com/colanconnon/apistar_alembic_migrations",
     description='Alembic migrations for apistar',
     author='Colan Connon',
     author_email='cconnon11@gmail.com',
-    packages=get_packages('apistar_alembic_migrations'),
-    package_data=get_package_data('apistar_alembic_migrations'),
+    packages=['apistar_alembic_migrations'],
     install_requires=[
         'alembic',
         'apistar'
