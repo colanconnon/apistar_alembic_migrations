3cdd59a1d01aee88ca8aa73c59c032ead789f2a4
colan connon <colanconnon@colans-mbp.lan>
2017-09-24 20:13:36 -0400

    started on alembic migrations

---
+++ apistar_alembic_migrations/__init__.py
@@ -0,0 +1,3 @@
+from .commands import create_revision, initialize, downgrade, upgrade, show
+
+__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+
+from setuptools import setup
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+version = get_version('apistar_alembic_migrations')
+
+
+setup(
+    name='apistar_alembic_migrations',
+    version='0.0.1',
+    license='MIT',
+    description='Alembic migrations for apistar',
+    author='Colan Connon',
+    author_email='cconnon11@gmail.com',
+    packages=get_packages('apistar_alembic_migrations'),
+    package_data=get_package_data('apistar_alembic_migrations'),
+    install_requires=[
+        'alembic',
+        'apistar'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
