6d954f06315ef174c5be9558f2087723dacb47f7
colan connon <cconnon11@gmail.com>
2017-09-24 20:36:02 -0400

    refactor to use apistar command structure

--- apistar_alembic_migrations/__init__.py
+++ apistar_alembic_migrations/__init__.py
@@ -1,3 +1,13 @@
 from .commands import create_revision, initialize, downgrade, upgrade, show
+from apistar import Command
 
-__version__ = '0.0.1'
+
+commands = [
+    Command('initialize', initialize),
+    Command('create_revision', create_revision),
+    Command('upgrade', upgrade),
+    Command('downgrade', downgrade),
+    Command('show', show)
+]
+
+__version__ = '0.0.3'
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='apistar_alembic_migrations',
-    version='0.0.2',
+    version='0.0.3',
     license='MIT',
     url="https://github.com/colanconnon/apistar_alembic_migrations",
     description='Alembic migrations for apistar',
