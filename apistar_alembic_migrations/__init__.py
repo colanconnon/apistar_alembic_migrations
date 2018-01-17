baafc9038e268d85068e3f0d21f1312b6f34475d
colan connon <cconnon11@gmail.com>
2018-01-17 07:35:01 -0500

    revert init

--- apistar_alembic_migrations/__init__.py
+++ apistar_alembic_migrations/__init__.py
@@ -9,14 +9,25 @@ PRELOAD = True
 LOADED = False
 MIGRATIONS_DIRECTORY = "migrations"
 
-def init_migrations():
-    try:
-        os.system("alembic init migrations")
-    catch:
-        print('please install alembic and run command "apistar initialize"')
+def _get_alembic_config(engine, config_file="alembic.ini"):
+    alembic_cfg = Config(config_file)
+    alembic_cfg.set_main_option("script_location",
+                                MIGRATIONS_DIRECTORY)
+    alembic_cfg.set_main_option("sqlalchemy.url", str(engine.url))
+    return alembic_cfg
+
+def _run_alembic_command(alembic_cmd: Callable, engine, metadata, *args, **kwargs):
+    alembic_cfg = _get_alembic_config(engine)
+    with engine.begin() as connection:
+        alembic_cfg.attributes['target_metadata'] = metadata
+        alembic_cfg.attributes['connection'] = connection
+        alembic_cmd(alembic_cfg, *args, **kwargs)
 
 def create_revision(db_backend: SQLAlchemyBackend, message: str):
     _run_alembic_command(command.revision,db_backend.engine, db_backend.metadata, message=message, autogenerate=True)
+
+def initialize(db_backend: SQLAlchemyBackend):
+    _run_alembic_command(command.init, db_backend.engine, db_backend.metadata, directory=MIGRATIONS_DIRECTORY)
 
 def downgrade(db_backend: SQLAlchemyBackend, revision):
     _run_alembic_command(command.downgrade, db_backend.engine, db_backend.metadata, revision=revision)
@@ -29,7 +40,7 @@ def show(db_backend: SQLAlchemyBackend, revision):
 
 
 commands = [
-    Command('initialize', init_migrations),
+    Command('initialize', initialize),
     Command('create_revision', create_revision),
     Command('upgrade', upgrade),
     Command('downgrade', downgrade),
